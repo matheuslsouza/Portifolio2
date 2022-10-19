fc6a6a48c6c12570f24f1d6394ce47be9e322298
Matheus L. Souza <107487181+matheuslsouza@users.noreply.github.com>
2022-10-19 11:05:17 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -39,8 +39,35 @@ const scrollHeader = () => {
 };
 
 /*=============== SWIPER PROJECTS ===============*/
+let swiperProjects = new Swiper(".projects__container", {
+  loop: true,
+  spaceBetween: 24,
 
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  //mousewheel: true,
+  //keyboard: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      spaceBetween: -56,
+    },
+  },
+});
 /*=============== SWIPER TESTIMONIAL ===============*/
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  grabCursor: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
 /*=============== EMAIL JS ===============*/
 
